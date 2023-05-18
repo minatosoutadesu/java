64722d77cfe2a526292e6b418c8edd488bac55a6
Your Name <you@example.com>
2023-05-18 19:05:03 +0900

    提出1回目

---
+++ app.js
@@ -0,0 +1,25 @@
+for(let i = 1; i < 51; i++){
+  console.log(i)
+  }
+
+if (i=10) {
+  console.log('今10回ループしました。');
+}
+
+if (i=20) {
+  console.log('今20回ループしました。');
+}
+
+if (i=30) {
+  console.log('今30回ループしました。');
+}
+
+if (i=40) {
+  console.log('今40回ループしました。');
+}
+
+if (i % 4 == 0) {
+  console.log('4で割れる数です。');
+}
+
+alert('50回カウントが終わりました。')
